86b15760631981767c6cdcd2cc3d1def8ef42857
Jeremy <jnguyenxuan@lbl.gov>
2017-01-26 16:23:10 -0800

    Trying to use data graph instead

--- files/run.sh
+++ files/run.sh
@@ -7,7 +7,8 @@ set -e
 /data/solr-6.2.1/bin/solr stop
 rm /data/solr-6.2.1/server/solr/search/conf/managed-schema 
 cd /data/golr-schema && mvn exec:java -Dexec.mainClass="org.bbop.cli.Main" -Dexec.args="-c /data/monarch-app/conf/golr-views/monarch-search-config.yaml -o /data/solr-6.2.1/server/solr/search/conf/schema.xml"
-wget -O /data/scigraph.tgz http://scigraph-ontology-dev.monarchinitiative.org/static_files/scigraph.tgz 
+#wget -O /data/scigraph.tgz https://scigraph-ontology-dev.monarchinitiative.org/static_files/scigraph.tgz 
+wget -O /data/scigraph.tgz https://scigraph-data-dev.monarchinitiative.org/static_files/scigraph.tgz 
 cd /data/ && tar xzfv scigraph.tgz 
 cd /data/golr-loader && mvn exec:java -Dexec.mainClass="org.monarch.golr.SimpleLoaderMain" -Dexec.args="-g /data/graph.yaml -o output.json"
 /data/solr-6.2.1/bin/solr start
