3bb7a86a9261e9a85b2aadd5eea87179a8d88ecd
kshefchek <kshefchek@gmail.com>
2019-12-30 09:51:08 -0800

    add schema yaml to repo

--- Dockerfile
+++ Dockerfile
@@ -12,10 +12,10 @@ WORKDIR /data
 ADD files/functions.inc /data/
 ADD files/run.sh /data/
 ADD files/prefix_equivalents.yaml /data/
+ADD files/monarch-search-config.yaml /data/
 
 RUN git clone https://github.com/SciGraph/SciGraph.git /data/scigraph
 RUN git clone https://github.com/SciGraph/golr-loader.git /data/golr-loader
-RUN git clone https://github.com/monarch-initiative/monarch-app.git /data/monarch-app
 RUN git clone https://github.com/monarch-initiative/golr-schema /data/golr-schema
 
 RUN cd /data/scigraph && mvn install -DskipTests -DskipITs
--- files/run.sh
+++ files/run.sh
@@ -6,8 +6,7 @@ set -e
 /data/solr-6.2.1/bin/solr create -c search 
 /data/solr-6.2.1/bin/solr stop
 rm /data/solr-6.2.1/server/solr/search/conf/managed-schema 
-cd /data/golr-schema && mvn exec:java -Dexec.mainClass="org.bbop.cli.Main" -Dexec.args="-c /data/monarch-app/conf/golr-views/monarch-search-config.yaml -o /data/solr-6.2.1/server/solr/search/conf/schema.xml"
-#wget -O /data/scigraph.tgz https://scigraph-ontology-dev.monarchinitiative.org/static_files/scigraph.tgz 
+cd /data/golr-schema && mvn exec:java -Dexec.mainClass="org.bbop.cli.Main" -Dexec.args="-c /data/monarch-search-config.yaml -o /data/solr-6.2.1/server/solr/search/conf/schema.xml"
 wget -O /data/scigraph.tgz https://scigraph-data-dev.monarchinitiative.org/static_files/scigraph.tgz 
 cd /data/ && tar xzfv scigraph.tgz 
 cd /data/golr-loader && mvn exec:java -Dexec.mainClass="org.monarch.golr.SimpleLoaderMain" -Dexec.args="-g /data/graph.yaml -m /data/prefix_equivalents.yaml -o output.json"
